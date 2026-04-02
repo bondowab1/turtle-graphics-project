84c8b3b6055fffe61cbdb836adb7fa5dd9d85cc7
mimo <bondowab1@learn.cssd.ab.ca>
2026-04-01 20:14:35 -0600

    Add spiral.py file to draw a spiral using turtle graphics

---
+++ spiral.py
@@ -0,0 +1,13 @@
+import turtle
+
+# Create screen and turtle
+screen = turtle.Screen()
+screen.title("Draw a Spiral")
+pn = turtle.Turtle()
+
+# Draw a spiral
+for i in range(100):
+    pen.forward(i)
+    pen.right(30)
+
+turtle.done()
